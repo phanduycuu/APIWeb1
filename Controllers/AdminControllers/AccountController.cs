77b7c574a9242ae5913b3e220ddf16c69631a50e
Tran Truong <chantruong753@gmail.com>
2024-11-10 19:47:36 +0700

    add template admin

---
+++ Controllers/AdminControllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIWeb1.Controllers.AdminControllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Employer()
+        {
+            return View();
+        }
+
+        public IActionResult JobSeekers()
+        {
+            return View();
+        }
+
+    }
+}
