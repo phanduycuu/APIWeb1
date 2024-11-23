04f6c0878a30102c08e13a5018ea36fd7c620b5e
phanduycuu <0cauchin0@gmail.com>
2024-11-23 16:53:24 +0700

    thong ke

---
+++ Controllers/AdminControllers/StatisticalController.cs
@@ -0,0 +1,19 @@
+﻿using APIWeb1.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIWeb1.Controllers.AdminControllers
+{
+    public class StatisticalController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public StatisticalController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
--- wwwroot/admin/js/main.js
+++ wwwroot/admin/js/main.js
@@ -1,4 +1,4 @@
-(function ($) {
+﻿(function ($) {
     "use strict";
 
     // Spinner
@@ -57,6 +57,64 @@
         loop: true,
         nav : false
     });
+
+    const ctx = document.getElementById('chart1');
+    const ctx1 = document.getElementById('chart2');
+    const labels = Utils.months({ count: 7 });
+    const data1 = {
+        labels: labels,
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }],
+    new Chart(ctx, {
+        type: 'bar',
+
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+
+    });
+
+
+
+    new Chart(ctx1, {
+        type: 'bar',
+
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+
+    });
+
+
 
 
 
