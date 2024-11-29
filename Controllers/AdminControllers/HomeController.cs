bf8a7c4bb4d99550aecdd1228b91be1b62afde25
Tran Truong <chantruong753@gmail.com>
2024-11-30 03:57:36 +0700

    add code

--- Controllers/AdminControllers/HomeController.cs
+++ Controllers/AdminControllers/HomeController.cs
@@ -1,16 +1,39 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using APIWeb1.Extensions;
+using APIWeb1.Interfaces;
+using APIWeb1.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APIWeb1.Controllers.AdminControllers
 {
     [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
-
-        public IActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEmailSender _emailSender;
+        public HomeController(IUnitOfWork unitOfWork, UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager, IEmailSender emailSender)
         {
-            
-            return View();
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _emailSender = emailSender;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var username = User.GetUsername();
+            var userInfo = await _userManager.Users
+                .Include(u => u.Company).Include(u => u.Address)
+                .FirstOrDefaultAsync(u => u.UserName == username);
+
+            ViewData["UserInfo"] = userInfo; // Hoặc ViewBag.UserInfo = userInfo;
+            return View(userInfo);
         }
 
 
